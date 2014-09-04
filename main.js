c5b6f569e9f50b56cc1c6ef559322202d88eefda
Jozef <jozefw@sbcglobal.net>
2014-09-04 15:19:21 -0700

    same app with jquery

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-function init() {
-	document.getElementById("c_to_f").onclick = celsius;
-	document.getElementById("f_to_c").onclick = faren;
-	
-	function celsius() {
-		var c = document.getElementById("to_faren").value;
+$(document).ready(function() {
+
+	var celsius = function() {
+		var c = $("#to_faren").val();
 		var f = (c * 1.8) + 32;
-		document.getElementById("C_answer").innerHTML = f;
-	}
+		$("#C_answer").html(f);
+	};
 
-	function faren() {
-		var f = document.getElementById("toCelsius").value;
+	$("#c_to_f").on("click", celsius);
+
+	var faren = function() {
+		var f = $("#toCelsius").val();
 		var c = (f - 32) / 1.8;
-		document.getElementById("F_answer").innerHTML = c;
-	}
-};
-
-window.onload = init();
+		$("#F_answer").html(c);
+	};
+	
+	$("#f_to_c").on("click", faren);
+});
