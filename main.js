98a5ed40a7bca884c5f36441642ae93bf5174089
Jozef <jozefw@sbcglobal.net>
2014-09-04 17:23:12 -0700

    buttonless jquery temp conversion

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
 	var celsius = function() {
 		var c = $("#to_faren").val();
 		var f = (c * 1.8) + 32;
-		$("#C_answer").html(f);
+		$("#toCelsius").val(f);
 	};
 
-	$("#c_to_f").on("click", celsius);
+	$("#to_faren").on("change", celsius);
 
 	var faren = function() {
 		var f = $("#toCelsius").val();
 		var c = (f - 32) / 1.8;
-		$("#F_answer").html(c);
+		$("#to_faren").val(c);
 	};
 	
-	$("#f_to_c").on("click", faren);
+	$("#toCelsius").on("change", faren);
 });
